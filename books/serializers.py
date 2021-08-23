edf774b693f1b3797eec7ad45bb478bbbe79a755
salik <saliksheraz786@gmail.com>
2021-08-24 00:09:45 +0500

    removed serializer

--- books/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-from books.models import Bookshelf
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Bookshelf
-        exclude = 'url'
